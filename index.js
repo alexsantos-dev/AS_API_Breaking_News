a4f210d5b80def862d99a82c1abb974e11b8544a
Alex Santos <alexsantos.web.developer@gmail.com>
2023-11-03 13:59:29 -0300

    organizando pastas e arquivos

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 const express = require("express")
 const app = express();
-const teste = express();
+const userRoute = require("./src/routes/user.route")
 
-app.get("/", (req, res) => {
-    res.send("Aoba Garai!")
-})
+app.use("/soma", userRoute)
 
-teste.get("/teste", (req, res) => {
-    res.send("Isso só pq eu tava testando ele ;)")
-})
-
-teste.get("/laele", (req, res) => {
-    const sum = 100 + 1
-    res.json({sum: sum})
-})
-
-teste.listen(3300)
 app.listen(3000)
