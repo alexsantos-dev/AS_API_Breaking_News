639a6ab0ddd4dd993ece22569ee24b182c7010b6
Alex Santos <alexsantos.web.developer@gmail.com>
2023-11-11 16:50:00 -0300

    adicionado recurso de paginação de notícias

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,4 @@
-import {createService, findAllService} from "../services/news.service.js"
+import {createService, findAllService, countNews, topNewsService} from "../services/news.service.js"
 
 const create = async (req, res) => {
     try{ const {title, text, banner} = req.body
@@ -23,17 +23,78 @@ const create = async (req, res) => {
 
 const findAll = async (req, res) =>{
     
-    try{ const news = await findAllService()
+ try{ let {limit, offset} = req.query
+
+    limit = Number(limit)
+    offset = Number(offset)
+
+    if(!limit){
+        limit = 5
+    }
+
+    if(!offset){
+        offset = 0
+    }
+        const news = await findAllService(offset, limit)
+        const total = await countNews()
+        const currentURL = req.baseUrl
+        const next = offset + limit
+        const nextURL = next < total ? `${currentURL}?limit=${limit}&offset=${next}` : null
+
+        const previous = offset - limit < 0 ? null :  offset -  limit
+        const previousURL = previous !== null ? `${currentURL}?limit=${limit}&offset=${previous}` : null
 
     if (news.length === 0) {
         return res.sendStatus(400, { message: "Não há usuários cadastrados!" })
     }
 
-    res.send(news)}
+    res.send({
+        nextURL,
+        previousURL,
+        limit,
+        offset,
+        total,
+        results: news.map(Item => ({
+            id: Item._id,
+            title: Item.title,
+            text: Item.text,
+            banner: Item.banner,
+            likes: Item.likes,
+            comments: Item.comments,
+            name: Item.user.name,
+            username: Item.user.username,
+            avatar: Item.user.avatar,
+        }))
+    })}
     
     catch(err){
         res.status(500).send({message: err.message})
     }
 }
 
-export {create, findAll} 
+const topNews = async (req, res) =>{
+   try{ const news = await topNewsService()
+    
+    if(!news){
+        return res.send(400).send({ message: "Não há usuários cadastrados!" })
+    }
+    res.send({
+            news:{
+                id: news._id,
+                title: news.title,
+                text: news.text,
+                banner: news.banner,
+                likes: news.likes,
+                comments: news.comments,
+                name: news.user.name,
+                username: news.user.username,
+                avatar: news.user.avatar,
+            },
+    })}
+
+    catch(err){
+        res.status(500).send({message: err.message})
+    }
+}
+
+export {create, findAll, topNews} 
