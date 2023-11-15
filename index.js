8aecd0d0777923129e53081c47b092e4ef186d85
Alex Santos <alexsantos.web.developer@gmail.com>
2023-11-15 20:05:37 -0300

    ajustando swagger.route em cjs

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import userRoute from "./src/routes/user.route.js"
 import authRoute from "./src/routes/auth.route.js"
 import newsRoute from "./src/routes/news.route.js"
 import dotenv from "dotenv"
-import swaggerRoute from "./src/routes/swagger.route.js"
+import swaggerRoute from "./src/routes/swagger.route.cjs"
 
 dotenv.config()
 
