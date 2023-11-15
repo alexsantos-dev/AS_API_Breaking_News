7f6d434e393733f8732383598cb99732d8c35d91
Alex Santos <alexsantos.web.developer@gmail.com>
2023-11-15 20:00:00 -0300

    assert removido em swagger.route

--- src/routes/swagger.route.js
+++ src/routes/swagger.route.js
@@ -2,7 +2,7 @@ import {Router, json} from "express"
 const router = Router()
 
 import swaggerUi from "swagger-ui-express"
-import swaggerDocument from "../services/swagger.json" assert {type: "json"}
+import swaggerDocument from "../services/swagger.json"
 
 router.use("/", swaggerUi.serve)
 router.get("/", swaggerUi.setup(swaggerDocument))
