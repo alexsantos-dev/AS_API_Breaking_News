65d3601778e8c12fb399e4f071b6f8db71fd988a
Alex Santos <alexsantos.web.developer@gmail.com>
2023-11-19 18:12:23 -0300

    habilitando o cors no backend

--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -1,21 +1,43 @@
-import {Router} from "express"
-const router = Router();
+import express from "express";
+import cors from "cors";
+const router = express.Router();
 
-import {create, findAll, findById, searchByTitle, topNews, byUser, update, erase, likeNews, addComment, deleteComment} from "../controllers/news.controller.js"
+import {
+    create,
+    findAll,
+    findById,
+    searchByTitle,
+    topNews,
+    byUser,
+    update,
+    erase,
+    likeNews,
+    addComment,
+    deleteComment,
+} from "../controllers/news.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 
-router.post("/", authMiddleware, create)
-router.get("/", findAll)
-router.get("/top", topNews)
-router.get("/search", searchByTitle)
-router.get("/byUser", authMiddleware, byUser)
-router.patch("/:id", authMiddleware, update)
-router.delete("/:id", authMiddleware, erase)
-router.patch("/like/:id", authMiddleware, likeNews)
-router.patch("/comment/:id", authMiddleware, addComment)
-router.patch("/comment/:idNews/:idComment", authMiddleware, deleteComment)
+// Configurar o middleware CORS para permitir apenas origens específicas
+const corsOptions = {
+    origin: 'http://localhost:5173', // Substitua pelo seu domínio permitido
+    methods: 'GET,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
 
+router.use(cors(corsOptions));
 
-router.get("/:id", authMiddleware, findById)
+router.post("/", authMiddleware, create);
+router.get("/", findAll);
+router.get("/top", topNews);
+router.get("/search", searchByTitle);
+router.get("/byUser", authMiddleware, byUser);
+router.patch("/:id", authMiddleware, update);
+router.delete("/:id", authMiddleware, erase);
+router.patch("/like/:id", authMiddleware, likeNews);
+router.patch("/comment/:id", authMiddleware, addComment);
+router.patch("/comment/:idNews/:idComment", authMiddleware, deleteComment);
 
-export default router
+router.get("/:id", authMiddleware, findById);
+
+export default router;
